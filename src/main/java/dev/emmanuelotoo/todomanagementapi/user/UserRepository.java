0b1a7b111020c80897c5ad925eec433e2d4003a7
Emmanuel <eotoo218@gmail.com>
2025-10-13 21:01:54 +0000

    add JWT authentication filter, JwtService, and update user repository with proper annotation

--- src/main/java/dev/emmanuelotoo/todomanagementapi/user/UserRepository.java
+++ src/main/java/dev/emmanuelotoo/todomanagementapi/user/UserRepository.java
@@ -1,11 +1,11 @@
 package dev.emmanuelotoo.todomanagementapi.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Component
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
 }
