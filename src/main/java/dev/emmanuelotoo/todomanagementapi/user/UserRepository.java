45f42e7b5fb3abb2ad299d8c069017a2c0484e7c
Emmanuel <eotoo218@gmail.com>
2025-10-13 14:30:09 +0000

    add JWT dependencies and create UserRepository interface for user data access

---
+++ src/main/java/dev/emmanuelotoo/todomanagementapi/user/UserRepository.java
@@ -0,0 +1,11 @@
+package dev.emmanuelotoo.todomanagementapi.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+}
