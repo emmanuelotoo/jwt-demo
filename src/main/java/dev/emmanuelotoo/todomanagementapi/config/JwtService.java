cc9801cebd8506b2cde00ae250405e1e2b83c9bd
Emmanuel <eotoo218@gmail.com>
2025-10-13 21:56:30 +0000

    implement JWT claim extraction logic, add secret key configuration, and enhance JwtService functionality

--- src/main/java/dev/emmanuelotoo/todomanagementapi/config/JwtService.java
+++ src/main/java/dev/emmanuelotoo/todomanagementapi/config/JwtService.java
@@ -1,10 +1,36 @@
 package dev.emmanuelotoo.todomanagementapi.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
 
 @Service
 public class JwtService {
+
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
     public String extractUsername(String token) {
         return null;
     }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
 }
